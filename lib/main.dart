9dbce823d07928eb339925f5eb0e13a348d7e176
Sujan Shrestha <sujanstha406@gmail.com>
2020-10-31 20:36:12 +0545

    List view implemented

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,44 @@
 import "package:flutter/material.dart";
 
-import './app_screen/home.dart';
+//import './app_screen/home.dart';
 
 void main() {
   runApp(MaterialApp(
-    title: "Exploring UI Widgets",
-    home: Home(),
     debugShowCheckedModeBanner: false,
+    title: "Exploring UI Widgets",
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text("Basic List View "),
+      ),
+      body: getListView(),
+    ),
   ));
+}
+
+Widget getListView() {
+  var listView = ListView(
+    children: [
+      ListTile(
+        leading: Icon(Icons.landscape),
+        title: Text("Landscape"),
+        subtitle: Text("Beautiful View !"),
+        trailing: Icon(Icons.wb_sunny),
+        onTap: () {
+          debugPrint("Landscape tapped");
+        },
+      ),
+      ListTile(
+        leading: Icon(Icons.laptop_chromebook),
+        title: Text("Google"),
+      ),
+      ListTile(
+        leading: Icon(Icons.phone_iphone),
+        title: Text("IOS"),
+      ),
+      Text("Can even add other elements in list"),
+      Container(color: Colors.amberAccent, height: 60.0)
+    ],
+  );
+
+  return listView;
 }
