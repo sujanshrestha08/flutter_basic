95cd98c1c90fbedde6bd17f49989c0cc1c9cd0c6
Sujan Shrestha <sujanstha406@gmail.com>
2020-11-01 20:21:29 +0545

    Covered FAB and snackbar Section

--- lib/app_screen/long_list_view.dart
+++ lib/app_screen/long_list_view.dart
@@ -1,5 +1,17 @@
 import "package:flutter/material.dart";
 
+void showSnackBar(BuildContext context, String item) {
+  var snackBar = SnackBar(
+    content: Text("You just tapped $item"),
+    action: SnackBarAction(
+        label: "UNDO",
+        onPressed: () {
+          debugPrint('Performing dummy UNDO operation');
+        }),
+  );
+  Scaffold.of(context).showSnackBar(snackBar);
+}
+
 List<String> getListElements() {
   var items = List<String>.generate(250, (counter) => "Item $counter");
   return items;
@@ -8,13 +20,13 @@ List<String> getListElements() {
 Widget getLongListView() {
   var listItems = getListElements();
   var listView = ListView.builder(itemBuilder: (context, index) {
-    return ListTile(
-      leading: Icon(Icons.arrow_forward_ios),
-      title: Text(listItems[index]),
-      onTap: () {
-        debugPrint('${listItems[index]} was tapped');
-      },
-    );
+    var listTile = ListTile(
+        leading: Icon(Icons.arrow_forward_ios),
+        title: Text(listItems[index]),
+        onTap: () {
+          showSnackBar(context, listItems[index]);
+        });
+    return listTile;
   });
 
   return listView;
--- lib/main.dart
+++ lib/main.dart
@@ -4,14 +4,23 @@ import './app_screen/long_list_view.dart';
 //import './app_screen/home.dart';
 
 void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    title: "Exploring UI Widgets",
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text("Long List View "),
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Exploring UI Widgets",
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Long List View "),
+        ),
+        body: getLongListView(),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            debugPrint("FAB Clicked");
+          },
+          child: Icon(Icons.add),
+          tooltip: 'Add One More Item',
+        ),
       ),
-      body: getLongListView(),
     ),
-  ));
+  );
 }
